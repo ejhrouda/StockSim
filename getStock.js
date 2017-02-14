fc78911f1ca17222188aced9dfae4f8995d9f986
ejhrouda <hroudaej@slu.edu>
2017-02-14 17:26:39 -0600

    Add files via upload

--- getStock.js
+++ getStock.js
@@ -1,7 +1,8 @@
 function StockPriceTicker() {
     'use strict';
     var ticker = document.getElementById("tickBox").value,
-        YQL_url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + ticker + "%22)&env=store://datatables.org/alltableswithkeys",
+        // YQL_url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + ticker + "%22)&env=store://datatables.org/alltableswithkeys",
+        YQL_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + ticker + "%22)&format=json&env=store://datatables.org/alltableswithkeys",
         StockTickerHTML = "",
         CompName,
         Price,
@@ -13,33 +14,21 @@ function StockPriceTicker() {
         stockhtmlTable,
         stockhtmlChart,
         Symbol,
+        Ask,
+        Bid,
         
-        StockTickerXML = $.get(YQL_url, function (xml) {
-            $(xml).find("quote").each(function () {
-                $(this).find("Symbol").each(function () {
-                    Symbol = $(this).text();
-                });
-                $(this).find("Name").each(function () {
-                    CompName = $(this).text();
-                });
-                $(this).find("LastTradePriceOnly").each(function () {
-                    Price = $(this).text();
-                });
-                $(this).find("Change").each(function () {
-                    ChnageInPrice = $(this).text();
-                });
-                $(this).find("PercentChange").each(function () {
-                    PercentChnageInPrice = $(this).text();
-                });
-                $(this).find("LastTradeTime").each(function () {
-                    Time = $(this).text();
-                });
-                $(this).find("Open").each(function () {
-                    Open = $(this).text();
-                });
-                $(this).find("PreviousClose").each(function () {
-                    PrevClose = $(this).text();
-                });
+        StockTickerXML = $.get(YQL_url, function (_return) {
+            var stock = _return.query.results.quote;
+            CompName = stock.Name;
+            Symbol = stock.symbol;
+            Price = stock.LastTradePriceOnly;
+            Time = stock.LastTradeTime;
+            ChnageInPrice = stock.Change;
+            PercentChnageInPrice = stock.Change_PercentChange;
+            Open = stock.Open;
+            PrevClose = stock.PreviousClose;
+            Ask = stock.Ask;
+            Bid = stock.Bid;
                 
                 stockhtmlTable = "<h2> Company Name: " + CompName + " (" + Symbol + ") </h2>";
                 stockhtmlTable = stockhtmlTable + "<table style='width:35%' align='center'> <tr>";
@@ -55,16 +44,19 @@ function StockPriceTicker() {
                 stockhtmlTable = stockhtmlTable + "<th>" + Open + "</th>";
                 stockhtmlTable = stockhtmlTable + "<th>Previous Close:</th>";
                 stockhtmlTable = stockhtmlTable + "<th>" + PrevClose + "</th></tr>";
+                stockhtmlTable = stockhtmlTable + "<tr><th>Ask:</th>";
+                stockhtmlTable = stockhtmlTable + "<th>" + Ask + "</th>";
+                stockhtmlTable = stockhtmlTable + "<th>Bid:</th>";
+                stockhtmlTable = stockhtmlTable + "<th>" + Bid + "</th></tr>";
                 stockhtmlTable = stockhtmlTable + "</table><br>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert1D('" + Symbol + "')\" id='1DButton'>1D</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert5D('" + Symbol + "')\" id='5DButton'>5D</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert1M('" + Symbol + "')\" id='1MButton'>1M</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert6M('" + Symbol + "')\" id='6MButton'>6M</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert1Y('" + Symbol + "')\" id='1YButton'>1Y</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert2Y('" + Symbol + "')\" id='2YButton'>2Y</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert5Y('" + Symbol + "')\" id='5YButton'>5Y</button>";
-                stockhtmlTable = stockhtmlTable + "<button onclick=\"insert9Y('" + Symbol + "')\" id='9YButton'>9Y</button>";
-            });
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert1D('" + Symbol + "')\" id='1DButton'>1D</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert5D('" + Symbol + "')\" id='5DButton'>5D</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert1M('" + Symbol + "')\" id='1MButton'>1M</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert6M('" + Symbol + "')\" id='6MButton'>6M</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert1Y('" + Symbol + "')\" id='1YButton'>1Y</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert2Y('" + Symbol + "')\" id='2YButton'>2Y</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert5Y('" + Symbol + "')\" id='5YButton'>5Y</button>";
+                stockhtmlTable = stockhtmlTable + "<button style=\"background-color: green;border: none;color: white;text-align: center;padding: 5px 20px;font-size: 16px;\" onclick=\"insert9Y('" + Symbol + "')\" id='9YButton'>9Y</button>";
 				
             document.getElementById("showStockTick").innerHTML = stockhtmlTable;
             insert6M(Symbol);
